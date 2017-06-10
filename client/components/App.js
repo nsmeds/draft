79865a72934f99fa47d332337da4fc5cc4ff59a8
nsmeds <niilosmeds@gmail.com>
2017-06-09 22:35:54 -0700

    add draft-js and webpack plugins

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Editor, EditorState } from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editorState: EditorState.createEmpty()
+        };
+        this.focus = () => this.refs.editor.focus();
+        this.onChange = (editorState) => this.setState({editorState});
+        this.logState = () => console.log(this.state.editorState.toJS());
+    }
     render() {
         return (
-            <div style={{textAlign: 'center'}}>
-                <h3>Here is where our editor will live, cool huh</h3>
+            <div className="root">
+                <div className="editor" onClick={this.focus}>
+                    <Editor 
+                        editorState={this.state.editorState}
+                        onChange={this.onChange}
+                        placeholder="Enter some text"
+                        ref="editor"
+                    />
+                </div>
+                <input
+                    onClick={this.logState}
+                    className="button"
+                    type="button"
+                    value="Log State"
+                />
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: './client/index.html',
@@ -17,7 +17,11 @@ module.exports = {
         loaders: [
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
             { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
+            { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })}
         ]
     },
-    plugins: [HtmlWebpackPluginConfig]
+    plugins: [
+        HtmlWebpackPluginConfig,
+        new ExtractTextPlugin('[name].css')
+        ]
 }
