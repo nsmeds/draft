8c764519e0c849ef187e05ad648e9ef7d5727420
nsmeds <niilosmeds@gmail.com>
2017-06-09 21:00:52 -0700

    initial skeleton

---
+++ client/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h3>Here is where our editor will live, cool huh</h3>
+            </div>
+        );
+    }
+}
